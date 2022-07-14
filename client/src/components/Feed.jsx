d2c9b6ad912cab5dec5a286fcfe3ae9f0c8b3cd3
iGorillaz <barannik.denis.92@gmail.com>
2022-07-14 19:25:45 +0300

    Layout added with some logic in feed and data for sanity

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -4,13 +4,38 @@ import { useParams } from "react-router-dom";
 
 import { client } from "../client";
 import { Spinner, MasonryLayout } from "../components";
+import { feedQuery, searchQuery } from "../utils/data";
 
 const Feed = () => {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [pins, setPins] = useState(null);
+  const { categoryId } = useParams();
+
+  useEffect(() => {
+    setLoading(true);
+
+    if (categoryId) {
+      const query = searchQuery(categoryId);
+
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [categoryId]);
 
   if (loading)
-    return <Spinner message="We are adding new ideas to your feed!" />;
-  return <div>Feed</div>;
+    return <Spinner message="lookon our spinner :)" />;
+  return (
+  <div>
+	{pins && <MasonryLayout pins={pins} />}
+	</div>
+  )
 };
 
 export default Feed;
